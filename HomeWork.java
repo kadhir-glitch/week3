383bd6fd068b150c8664d8e76c538f90c39b9db9
abdulkhaliq784 <khaliqsaad18@gmail.com>
2024-12-12 11:34:43 +0530

    Add files via upload

---
+++ HomeWork.java
@@ -0,0 +1,22 @@
+package firstClass;
+public class HomeWork {
+	public static void main(String[] args) {
+		
+		String nam = "kadhir";
+		int age = 21;
+		char initial = 'M';
+		float cgp = 8.1f;
+		boolean result = true;
+		long ph= 987456123l;
+		
+		//       System.out.println(nam);
+		//       System.out.println(age);
+		//       System.out.println(initial);
+	    //       System.out.println(cgp);
+		//       System.out.println(result);
+		//       System.out.println(ph);
+        
+		System.out.println(nam +"\n"+age+"\n" +initial+"\n"+cgp+"\n"+result+" \n"+ph);
+	}
+
+}
